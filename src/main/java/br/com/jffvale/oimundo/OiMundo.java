15e42d937cf080ddcb0b93562d1a973f5dc24505
jffvale <joao.freitas15@fatec.sp.gov.br>
2024-06-17 14:38:49 -0300

    first commit

---
+++ src/main/java/br/com/jffvale/oimundo/OiMundo.java
@@ -0,0 +1,11 @@
+package br.com.jffvale.oimundo;
+
+/**
+ * @author João <your.name at your.org>
+ */
+public class OiMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
